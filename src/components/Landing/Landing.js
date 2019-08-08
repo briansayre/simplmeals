7c851ede97e5711a52056406124366f582974460
Brian Sayre <briansayre01@gmail.com>
2019-08-08 00:55:33 -0500

    Formatted all code correctly

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,65 +1,54 @@
-import React from 'react';
-import './Landing.css';
-import create from '../../images/create.png';
-import plan from '../../images/plan.png';
-import shop from '../../images/shop.png';
-import enjoy from '../../images/enjoy.png';
+import React from "react";
+import "./Landing.css";
+import create from "../../images/create.png";
+import plan from "../../images/plan.png";
+import shop from "../../images/shop.png";
+import enjoy from "../../images/enjoy.png";
 
 class Landing extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
 
-    constructor(props) {
-        super(props);
-        this.state = {
-        }
-    }
+	render() {
+		return (
+			<div className="landing">
+				<div className="header-image" />
 
-    render() {
-        return (
-            
-            <div className="landing">
-                
-                <div className="header-image">
-    
-                </div>
-            
-                <div className="landing-container">
+				<div className="landing-container">
+					<div className="images">
+						<div className="individual-images">
+							<img src={create} alt="" />
+							<h1 className="image-captions">Create.</h1>
+						</div>
 
-                    <div className="images">
+						<div className="individual-images">
+							<img src={plan} alt="" />
+							<h1 className="image-captions">Plan.</h1>
+						</div>
 
-                        <div className="individual-images">
-                            <img src={create} alt=""/>
-                            <h1 className="image-captions">Create.</h1>
-                        </div>
+						<div className="individual-images">
+							<img src={shop} alt="" />
+							<h1 className="image-captions">Shop.</h1>
+						</div>
 
-                        <div className="individual-images">
-                            <img src={plan} alt="" />
-                            <h1 className="image-captions">Plan.</h1>
-                        </div>
+						<div className="individual-images">
+							<img src={enjoy} alt="" />
+							<h1 className="image-captions">Enjoy.</h1>
+						</div>
+					</div>
 
-                        <div className="individual-images">
-                            <img src={shop} alt="" />
-                            <h1 className="image-captions">Shop.</h1>
-                        </div>
-
-                        <div className="individual-images">
-                            <img src={enjoy} alt="" />
-                            <h1 className="image-captions">Enjoy.</h1>
-                        </div >
-                        
-                    </div>
-
-                    <div>
-                        <p className="explain">
-                            Simplmeals allows you to create your own recipes<br /> and plan every meal quickly and simply.
-                        </p>
-                    </div>
-
-                </div>
-
-            </div>
-        );
-    }
+					<div>
+						<p className="explain">
+							Simplmeals allows you to create your own recipes
+							<br /> and plan every meal quickly and simply.
+						</p>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
-
 
 export default Landing;
